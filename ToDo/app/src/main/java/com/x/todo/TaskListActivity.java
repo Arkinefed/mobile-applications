ab884ed45e769acec72090a3970c7e44f5d2b68f
Arkinefed <arkinefedmail@gmail.com>
2023-05-25 12:24:04 +0200

    updated top bar

--- ToDo/app/src/main/java/com/x/todo/TaskListActivity.java
+++ ToDo/app/src/main/java/com/x/todo/TaskListActivity.java
@@ -1,11 +1,14 @@
 package com.x.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -18,6 +21,11 @@ public class TaskListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_list);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.tasks);
 
         RecyclerView taskList = findViewById(R.id.tasks_list);
 
@@ -47,5 +55,14 @@ public class TaskListActivity extends AppCompatActivity {
             AddTaskDialogFragment addTaskDialogFragment = new AddTaskDialogFragment(tasks, locationsAdapter, this);
             addTaskDialogFragment.show(getSupportFragmentManager(), "add_task_dialog");
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+
+        inflater.inflate(R.menu.menu, menu);
+
+        return true;
     }
 }
